883b50f42fec75b213baea7061f931dd7d581d59
raingo00 <31068110+raingo00@users.noreply.github.com>
2025-07-23 11:06:20 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// 자동 생성된 코드입니다. 편집하지 마십시오.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency18 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency18":return transparency18;
+        }
+        return null;
+    })
+
+}
+// 자동 생성된 코드입니다. 편집하지 마십시오.
